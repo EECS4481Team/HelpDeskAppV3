eb7b2c8ff4ce8d11d08e0ca48d604cfe9389b2f8
Andrew Vu <vuandre1@my.yorku.ca>
2023-03-24 22:05:13 -0400

    Changing to Jay's Firebase

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -1,32 +1,19 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-
-  apiKey: "AIzaSyD9eyqqEfY13qcjmgiQsFFGrJIIBxfXb0c",
-
-  authDomain: "uploadingfile-b8e4e.firebaseapp.com",
-
-  projectId: "uploadingfile-b8e4e",
-
-  storageBucket: "uploadingfile-b8e4e.appspot.com",
-
-  messagingSenderId: "416440469304",
-
-  appId: "1:416440469304:web:4220a434580594a7f363cd"
-
+  apiKey: "AIzaSyCFhlv2doIrEsbvZ7kDLcNzArKmCA-5muc",
+  authDomain: "uploadingfile-118d3.firebaseapp.com",
+  projectId: "uploadingfile-118d3",
+  storageBucket: "uploadingfile-118d3.appspot.com",
+  messagingSenderId: "47555293656",
+  appId: "1:47555293656:web:8f8ff67201ffd41b3882ad"
 };
 
-
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
